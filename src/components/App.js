c2ca1000e55468793a553b9fac674a54d6fb10b9
BalaChandraAkula <balachandraakula698@gmail.com>
2023-06-27 15:08:18 +0530

    submitting code-Tue Jun 27 2023 15:08:16 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[value, setValue] = useState({name:"", password:"", error:""})
+
+  const validate = ()=>{
+    if(value.name==="" || value.password===""){
+      setValue({...value, error: "Both username and password are required."})
+    } 
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>
+        <label htmlFor="name">UserName:</label>
+        <input type="text" onChange={(e)=>setValue({...value, name:e.target.value})}/>
+        </p>
+        <p>
+        <label htmlFor="pass">Password:</label>
+        <input type="password" onChange={(e)=>setValue({...value, password:e.target.value})}/>
+        </p>
+        {
+          value.error && value.error!="" &&
+            <p id="errorMessage">{value.error}</p>
+        }
+        
+        <button type="submit" onClick={validate}>Login</button>
     </div>
   )
 }
